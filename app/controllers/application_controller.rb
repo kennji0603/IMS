6879742084f817f564db115eb58ec54a8318212a
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-07-09 15:15:35 +0900

    ログイン後の条件分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    if  current_user.try(:admin?)
+       dashboard_path
+    else
+        root_path
+    end
+  end
 
+    def after_sign_out_path_for(resource)
+        new_user_session_path
+    end
   
   private
   def configure_permitted_parameters

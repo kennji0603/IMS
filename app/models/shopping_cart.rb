6b16f237b4cf920ad533a1ae2d3eb282f927b643
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-07-12 17:22:23 +0900

    ビジネスロジックの定義

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -3,6 +3,9 @@ class ShoppingCart < ApplicationRecord
   scope :set_user_cart, -> (user) { user_cart = where(user_id: user.id, buy_flag: false)&.last
     user_cart.nil? ? ShoppingCart.create(user_id: user.id)
                    : user_cart }
+                   scope :bought_carts, -> { where(buy_flag: true) }
+                   scope :search_carts_by_user, -> (user) { where(user_id: user) }
+                   scope :search_bought_carts_by_user, -> (user) { bought_carts.search_carts_by_user(user) }                  
 
 
   def tax_pct
